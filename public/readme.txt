c6ca8bbab85c0538e64dc8cc0c52100c47ba564d
John Aughey <jha@aughey.com>
2017-12-25 16:58:31 -0600

    'foo'

---
+++ public/readme.txt
@@ -0,0 +1,3 @@
+This Design Pack is copyright creativity103.com
+Images are licensed under a creative commons attribution license, credit 'creativity103.com'
+See the site for password and full details on terms of use.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
-import './App.css';
+//import './App.css';
 import './paginate.css';
 import Header from './Header'
 import jquery from 'jquery'
-import {Card, CardHeader, CardTitle, CardText} from 'material-ui/Card';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import TextField from 'material-ui/TextField'
-import Pagination from 'material-ui-pagination';
+//import {Card, CardHeader, CardTitle, CardText} from 'material-ui/Card';
+//import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+//import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+//import getMuiTheme from 'material-ui/styles/getMuiTheme'
+//import TextField from 'material-ui/TextField'
+//import Pagination from 'material-ui-pagination';
 import ReactPaginate from 'react-paginate';
-import RaisedButton from 'material-ui/RaisedButton';
+//import RaisedButton from 'material-ui/RaisedButton';
 import {BrowserRouter as Router, Link, Route, Switch, Redirect} from 'react-router-dom'
 
+/*
 class PlayRecording extends React.PureComponent {
   constructor(props) {
     super(props);
@@ -34,21 +35,45 @@ class PlayRecording extends React.PureComponent {
     return video;
   }
 }
+*/
 
 class Recording extends React.PureComponent {
   render() {
     var r = this.props.recording;
-    return (
-      <div className="recording"><CardTitle title={r.title} subtitle={r.speaker}/>
-        <PlayRecording session={this.props.session} recording={r}/>
+
+    var video = null;
+
+    var inner_content = (
+      <div>
+        <h1>{r.title}</h1>
+        <h2>{r.speaker}</h2>
       </div>
     )
+
+    if (this.props.showVideo) {
+      video = <iframe title={this.props.recording.youtube_url} width="560" height="315" src={this.props.recording.youtube_url} frameBorder="0" allowFullScreen></iframe>
+
+      return (
+        <div className="recording text-center">
+            {inner_content}
+            {video}
+        </div>
+      )
+    } else {
+      return (
+        <div className="recording text-center">
+          <Link to={`/r/${this.props.session._id}/0`}>
+            {inner_content}
+          </Link>
+        </div>
+      )
+    }
   }
 }
 
 class Recordings extends React.PureComponent {
   render() {
-    var r = this.props.recordings.map((r, i) => (<Recording key={i} session={this.props.session} recording={r}/>))
+    var r = this.props.recordings.map((r, i) => (<Recording key={i} showVideo={this.props.showVideo} session={this.props.session} recording={r}/>))
     return (
       <div className="recordings">
         {r}
@@ -58,28 +83,55 @@ class Recordings extends React.PureComponent {
 }
 
 class Session extends React.PureComponent {
+  /*
+  <Card className="session">
+    <CardHeader title={s.title} subtitle={s.description}/>
+
+    <CardText>
+      <Recordings session={s} recordings={s.recordings}/>
+    </CardText>
+  </Card>
+  */
   render() {
     var s = this.props.session;
+    //<img className="card-img-top" src={"/backgrounds/" + this.props.background + ".jpg"} alt=""/>
     return (
-      <Card className="session">
-        <CardHeader title={s.title} subtitle={s.description}/>
+      <div className={"session card h-100 background" + this.props.background}>
 
-        <CardText>
-          <Recordings session={s} recordings={s.recordings}/>
-        </CardText>
-      </Card>
+        <div className="card-body">
+          <Recordings showVideo={this.props.showVideo} session={s} recordings={s.recordings}/>
+          <div class='bottom'>
+            <small>
+              {s.title}
+              - {s.description}
+            </small>
+          </div>
+        </div>
+      </div>
     )
   }
 }
 
 class Sessions extends React.PureComponent {
   render() {
-    var s = this.props.sessions.map((s) => (<Session key={s._id} session={s}/>))
+
+    var s = this.props.sessions.map((s, i) => (
+      <div className="col-lg-4 col-sm-8"><Session key={s._id} session={s} background={i % 8}/></div>
+    ))
 
     return (
-      <div className="sessions">
+      <div className="card-group">
         {s}
       </div>
+    )
+  }
+}
+
+class SingleRec extends React.PureComponent {
+  render() {
+    console.log(this.props)
+    return (
+      <div className="col-12"><Session showVideo={true} background={0} session={this.props.data.sessions[0]}/></div>
     )
   }
 }
@@ -87,8 +139,8 @@ class Sessions extends React.PureComponent {
 class RecRoute extends React.Component {
   render() {
     return (
-      <LoadPageData id={this.props.match.params.sessionID} >
-        <Rec/>
+      <LoadPageData id={this.props.match.params.sessionID}>
+        <SingleRec/>
       </LoadPageData>
     )
   }
@@ -98,15 +150,15 @@ class LoadPageData extends React.Component {
   constructor(props) {
     super(props);
     this.state = {}
-    this.attrs = ['id','q','page']
+    this.attrs = ['id', 'q', 'page']
   }
   componentDidMount() {
     this.get(this.props);
   }
   componentWillReceiveProps(nextProps) {
-    for(var i in this.attrs) {
+    for (var i in this.attrs) {
       var a = this.attrs[i];
-      if(this.props[a] !== nextProps[a]) {
+      if (this.props[a] !== nextProps[a]) {
         this.get(nextProps);
         return;
       }
@@ -114,22 +166,18 @@ class LoadPageData extends React.Component {
   }
   get(props) {
     var url = "/sessions";
-    this.setState({
-      loading: true,
-      data: null
-    })
+    this.setState({loading: true, data: null})
 
     var args = {}
     this.attrs.forEach((a) => {
-      if(props[a]) {
+      if (props[a]) {
         args[a] = props[a];
       }
     })
+    args['limit'] = 9;
 
     jquery.getJSON(url, args).then((d) => {
-      this.setState({
-        data: d
-      })
+      this.setState({data: d})
       console.log(d);
       //this.setState(d)
       //return d;
@@ -140,9 +188,9 @@ class LoadPageData extends React.Component {
     });
   }
   render() {
-    if(this.state.data) {
+    if (this.state.data) {
       var children = this.props.children
-      var childrenWithProps = React.Children.map(children, child => React.cloneElement(child, { data: this.state.data }))
+      var childrenWithProps = React.Children.map(children, child => React.cloneElement(child, {data: this.state.data}))
       return (
         <div>{childrenWithProps}</div>
       )
@@ -166,15 +214,12 @@ class LoadPageData extends React.Component {
 
 class Rec extends React.PureComponent {
   render() {
-    return (
-        <Session session={this.props.data.sessions[0]} />
-      )
+    return (<Session session={this.props.data.sessions[0]}/>)
   }
 }
 
-
 class SearchInput extends Component {
-  search = (e,q) => {
+  search = (e, q) => {
     console.log(e);
     this.q = q;
   }
@@ -185,73 +230,8 @@ class SearchInput extends Component {
   render() {
     return (
       <div>
-        <TextField hintText="Search" onChange={this.search}/><button onClick={this.doSearch}>Search</button>
-      </div>
-    )
-  }
-}
-
-class GetContent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-    this.page = 1;
-  }
-  get() {
-    var q = this.props.q;
-    var url = "/sessions";
-    this.setState({
-      loading: true
-    })
-
-    jquery.getJSON(url, {page: this.page, q: q}).then((d) => {
-      console.log(d);
-      this.setState(d)
-      return d;
-    }).fail(() => {
-      this.setState({error: "Error: couldn't retrieve recordings.  Please contact John Aughey jha@aughey.com"})
-    }).always(() => {
-      this.setState({loading: false})
-      if (this.fetch_again) {
-        var q = this.fetch_again;
-        delete this.fetch_again;
-        this.get(q);
-        return;
-      }
-    });
-  }
-  changePage = (page) => {
-    console.log(page);
-    this.props.history.push("/page/" + page);
-  }
-  componentDidMount() {
-    this.get();
-  }
-  render() {
-    if (this.state.loading) {
-      return (
-        <div><img alt="loading" src="/ajax-loader.gif"/></div>
-      )
-    }
-
-    var pages = Math.ceil(this.state.count / this.state.limit);
-    var pagination = null;
-    if (pages > 1) {
-      pagination = (
-        <div>
-          Page {this.state.page}
-          of {pages}
-          <ReactPaginate forcePage={this.state.page - 1} breakLabel={<a>...</a>} breakClassName="break-me" containerClassName="pagination" subContainerClassName="pages pagination" activeClassName="active" pageCount={pages} display={10} current={this.state.page} onPageChange={this.changePage}/>
-        </div>
-      )
-    }
-
-    return (
-      <div className="content">
-        {pagination}
-        <Sessions sessions={this.state.sessions}/> {pagination}
+        <input hintText="Search" onChange={this.search}/>
+        <button onClick={this.doSearch}>Search</button>
       </div>
     )
   }
@@ -262,9 +242,8 @@ class Search extends React.PureComponent {
     var params = this.props.match.params;
     return (
       <div>
-      <div>Search Term: {params.search}</div>
-      <GetContent q={params.search}/>
-    </div>
+        <div>Search Term: {params.search}</div>
+      </div>
     )
   }
 }
@@ -275,19 +254,14 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <div className="app container">
-          <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-            <div className="row">
-              <Header/>
-              <Switch>
-              <Route path="/r/:sessionID/:recordingID" component={RecRoute}/>
-              <Route path="/search/:search" component={Search}/>
-              <Route path="/page/:page" component={RoutePage}/>
-              <Route path="*" component={PageRedirect}/>
-            </Switch>
-            </div>
-          </MuiThemeProvider>
-        </div>
+
+        <Switch>
+          <Route path="/r/:sessionID/:recordingID" component={RecRoute}/>
+          <Route path="/search/:search" component={Search}/>
+          <Route path="/page/:page" component={RoutePage}/>
+          <Route path="*" component={PageRedirect}/>
+        </Switch>
+
       </Router>
     );
   }
@@ -295,35 +269,31 @@ class App extends Component {
 
 class PageRedirect extends React.PureComponent {
   render() {
-    return (
-      <Redirect to="/page/1"/>
-    )
+    return (<Redirect to="/page/1"/>)
   }
 }
 
 class PaginatedSessions extends React.PureComponent {
   changePage = (page) => {
-    this.props.history.push("/page/" + (page.selected+1));
+    this.props.history.push("/page/" + (page.selected + 1));
   }
 
   render() {
     var data = this.props.data;
     var pages = Math.ceil(data.count / data.limit);
     var pagination = null;
+    //{"Page " + this.props.page + " of " + pages}
     if (pages > 1) {
       pagination = (
         <div>
-          Page {this.props.page}
-          of {pages}
-          <ReactPaginate forcePage={this.props.page - 1} breakLabel={<a>...</a>} breakClassName="break-me" containerClassName="pagination" subContainerClassName="pages pagination" activeClassName="active" pageCount={pages} display={10} current={this.props.page} onPageChange={this.changePage}/>
+          <ReactPaginate forcePage={this.props.page - 1} breakLabel={< a > ...</a>} breakClassName="break-me" containerClassName="pagination justify-content-center" pageClassName="page-item" pageLinkClassName="page-link" activeClassName="active" pageCount={pages} display={10} current={this.props.page} onPageChange={this.changePage}/>
         </div>
       )
     }
 
     return (
       <div>
-        {pagination}
-        <Sessions sessions={data.sessions}/>
+        <Sessions sessions={data.sessions}/> <hr/>{pagination}
       </div>
     )
   }
